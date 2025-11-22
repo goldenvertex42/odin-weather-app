a2b4da9aa7c1a53fa2e3958570258f0ff69010f3
goldenvertex42 <fabled93@gmail.com>
2025-11-21 18:46:37 -0600

    Download and configure html loader for loading svg

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,10 @@
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
